2058024a52b133bb55078f24ac845e47d3cc515d
Brahm Van Houzen <brahmvanh@gmail.com>
2024-06-03 15:31:45 -0400

    updated 404 page and set app to display that

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,49 +1,24 @@
-import * as React from "react"
-import { Link, HeadFC, PageProps } from "gatsby"
+import * as React from 'react';
+import { HeadFC } from 'gatsby';
 
-const pageStyles = {
-  color: "#232129",
-  padding: "96px",
-  fontFamily: "-apple-system, Roboto, sans-serif, serif",
-}
-const headingStyles = {
-  marginTop: 0,
-  marginBottom: 64,
-  maxWidth: 320,
-}
+import '../styles/NotFound.css';
 
-const paragraphStyles = {
-  marginBottom: 48,
-}
-const codeStyles = {
-  color: "#8A6534",
-  padding: 4,
-  backgroundColor: "#FFF4DB",
-  fontSize: "1.25rem",
-  borderRadius: 4,
-}
+import data_cable_install from '../images/data-cable-wiring.jpeg';
 
-const NotFoundPage: React.FC<PageProps> = () => {
-  return (
-    <main style={pageStyles}>
-      <h1 style={headingStyles}>Page not found</h1>
-      <p style={paragraphStyles}>
-        Sorry 😔, we couldn’t find what you were looking for.
-        <br />
-        {process.env.NODE_ENV === "development" ? (
-          <>
-            <br />
-            Try creating a page in <code style={codeStyles}>src/pages/</code>.
-            <br />
-          </>
-        ) : null}
-        <br />
-        <Link to="/">Go home</Link>.
-      </p>
-    </main>
-  )
-}
+const NotFoundPage: React.FC = () => {
+	return (
+		<main className='not-found'>
+			<div>
+				<img src={data_cable_install} alt='Data cable wiring' />
+				<div className='not-found-card-text'>
+					<h1>Sorry...</h1>
+					<p>Were working on some updates to the website right now, please do come back soon!</p>
+				</div>
+			</div>
+		</main>
+	);
+};
 
-export default NotFoundPage
+export default NotFoundPage;
 
-export const Head: HeadFC = () => <title>Not found</title>
+export const Head: HeadFC = () => <title>Not found</title>;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,16 +5,18 @@ import Services from '../components/Services';
 
 import '../styles/Home.css';
 import Layout from '../components/layout';
+import NotFoundPage from './404';
 
 const Home: React.FC = () => {
 	return (
-		<Layout>
-			<div className='home'>
-				<Services />
-				<Reviews />
-				<ContactHero />
-			</div>
-		</Layout>
+		// <Layout>
+		// 	<div className='home'>
+		// 		<Services />
+		// 		<Reviews />
+		// 		<ContactHero />
+		// 	</div>
+		// </Layout>
+	<NotFoundPage />
 	);
 };
 
