2ca20db3f1712fb8e1bda6cb92dffb1b5d49cb01
Brahm Van Houzen <brahmvanh@gmail.com>
2024-07-21 22:01:52 -0400

    thta was dumb

--- src/gatsby-plugin-apollo/client.ts
+++ src/gatsby-plugin-apollo/client.ts
@@ -17,7 +17,7 @@ const errorLink = onError(({ graphQLErrors, networkError }) => {
 	}
 });
 
-const httpLink = new HttpLink({ uri: process.env.GATSBY_LAMBDA_FUNCTION_URL, credentials: 'include', headers: { origin: window.location.origin } });
+const httpLink = new HttpLink({ uri: process.env.GATSBY_LAMBDA_FUNCTION_URL, credentials: 'include', headers: { origin: process.env.GATSBY_ORIGIN } });
 
 const link = from([errorLink, httpLink]);
 
