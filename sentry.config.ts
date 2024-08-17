807a476c3d5abdb0bb319a6d1ab7cfcacb1f581a
Brahm Van Houzen <brahmvanh@gmail.com>
2024-08-17 12:05:33 -0400

    updated sentry config

--- sentry.config.ts
+++ sentry.config.ts
@@ -3,7 +3,7 @@ import * as Sentry from '@sentry/gatsby';
 Sentry.init({
 	dsn: 'https://ea33d5cda2ee92cc4d5df4c95cf7ee75@o4506334305714176.ingest.us.sentry.io/4507784585871360',
 
-	integrations: [Sentry.browserTracingIntegration(), Sentry.replayIntegration()],
+	integrations: [Sentry.browserTracingIntegration(), Sentry.replayIntegration(), Sentry.browserProfilingIntegration()],
 
 	// Set tracesSampleRate to 1.0 to capture 100%
 	// of transactions for tracing.
