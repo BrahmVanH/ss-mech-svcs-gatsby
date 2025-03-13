98a84ccf9238a75e2c5ff9a9c219a86785ca0724
Brahm Van Houzen <brahmvanh@gmail.com>
2025-03-12 21:55:43 -0400

    changed nav bg color to primary blue instead of white.

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -7,12 +7,9 @@ import { ChevronDownIcon } from 'evergreen-ui';
 import brand_image_transparent from '../images/svg/brand-image-black-cropped.svg';
 import brand_image_white from '../images/svg/brand-image-white-cropped.svg';
 
-
 const Nav: React.FC = () => {
-
-
 	return (
-		<nav  className='bg-[#fff] border-b-2 border-primary  flex justify-center sm:justify-between items-center w-full text-black z-[1000] p-2 absolute'>
+		<nav className='bg-[#fff] bg-primary  flex justify-center sm:justify-between items-center w-full text-black z-[1000] p-2 absolute'>
 			<Link to='/' className=' hidden sm:flex justify-center items-center lg:ml-4'>
 				<img className='sm:hidden md:block md:w-[90px] mr-2' alt='south shore mechanical services branding its of a beautiful berner-aussie mix' src={brand_image_transparent} />
 
@@ -25,14 +22,14 @@ const Nav: React.FC = () => {
 					</span>
 				</div>
 			</Link>
-			<div className=' flex flex-col xs:flex-row justify-evenly sm:justify-end items-center w-full my-2 sm:w-[60%] m-0 sm:mr-8 text-[24px] sm:text-[18px] text-center sm"text-left [&>div>a]:mb-2 sm:[&>div>a]:text-3xl'>
-				<div >
-					<Link className='mb-4 no-underline p-4  hover:w-min hover:border-b-2 hover:border-[#ffffff3f]' to='/'>
+			<div className=' flex flex-col xs:flex-row justify-evenly sm:justify-end items-center w-full my-2 sm:w-[60%] m-0 sm:mr-8 text-[24px] sm:text-[18px] text-center sm:text-left font-bolder [&>div>a]:mb-2 sm:[&>div>a]:text-3xl'>
+				<div>
+					<Link className='mb-4 no-underline p-4  hover:w-min hover:border-b-2 hover:border-[#ffffff3f] font-bold' to='/'>
 						Home
 					</Link>
 				</div>
 
-				<div  className='z-[1000] group '>
+				<div className='z-[1000] group '>
 					<span className='flex row flex-nowrap transition-all ease-in-out duration-1000  no-underline   py-0 px-4 '>
 						<p className='text-[24px] sm:text-3xl'>Services</p>
 						<ChevronDownIcon className=' ml-2 mb-2 sm:mb-1 max-h-[12px] self-end justify-self-center' size={12} />
@@ -41,19 +38,19 @@ const Nav: React.FC = () => {
 					<div
 						id='servicesDropdownContent'
 						className='hidden absolute w-full sm:w-min min-w-min  flex-col justify-center items-center bg-[#fff] border-b-2 border-l-2 border-r-2 border-primary transition-all ease-in-out duration-1000 cursor-pointer z-[1000] rounded-b-[10px] py-4 left-auto sm:left-auto group-hover:flex'>
-						<div className='px-2 py-2' >
+						<div className='px-2 py-2'>
 							<Link className='mb-4 no-underline px-4 py-2 text-[24px] sm:text-3xl  hover:w-min hover:border-b-2 hover:border-[#ffffff3f]' to='/services/Commercial'>
 								Commercial
 							</Link>
 						</div>
-						<div className='px-2 py-2' >
+						<div className='px-2 py-2'>
 							<Link className='mb-4  no-underline px-4 py-2 text-[24px] sm:text-3xl  hover:w-min hover:border-b-2 hover:border-[#ffffff3f]' to='/services/Residential'>
 								Residential
 							</Link>
 						</div>
 					</div>
 				</div>
-				<div >
+				<div>
 					<Link className='mb-4  no-underline p-4  hover:w-min hover:border-b-2 hover:border-[#ffffff3f]' to='/Contact'>
 						Contact
 					</Link>
